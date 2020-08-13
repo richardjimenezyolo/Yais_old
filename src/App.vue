06a0c02e899155d4dee33ca497d881e80fbc1343
Richard <richardjimenez176@gmail.com>
2020-08-12 21:11:45 -0400

    now you can move between weeks in the calendar and addd moment.js to easy dates management

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
 	<ion-app>
-	  <router-view/>
+	  	<router-view/>
 	</ion-app>
 </template>
-
-<script>
-	export default {
-		created() {
-			console.clear()
-		}
-	}
-</script>
 
 <style>
 	body,html{
 		background: #000;
 	}
 	.bg{
+		background: #000;
 		--background: #000;
 		--color: white;
 	}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,14 @@
       <ion-title>
         Yais
       </ion-title>
+
+      <ion-button slot="end" color="dark" @click="moveLeft"><ion-icon name="caret-back-outline"></ion-icon></ion-button>
+      <ion-button slot="end" color="dark" @click="moveRight"><ion-icon name="caret-forward-outline"></ion-icon></ion-button>
+
     </ion-toolbar>
 
     <v-app>
-      <v-calendar :events="events" color="purple" :dark="dark" :type="mode" @click:event="showEvent"></v-calendar>
+      <v-calendar :events="events" color="purple" :value="today" :dark="dark" :type="mode" @click:event="showEvent"></v-calendar>
     </v-app>
   
     <ion-fab vertical="bottom" horizontal="end" slot="fixed">
@@ -24,16 +28,17 @@
 </template>
 
 <script>
+  import moment from 'moment';
+
   export default {
     created() {
-      console.clear()
       const lts = JSON.parse(JSON.stringify(localStorage))
-      console.log(lts)
+      // console.log(lts)
 
       for (var i in lts) {
         try {
           const cita = JSON.parse(localStorage.getItem(i))
-          console.log(cita)
+          // console.log(cita)
           this.events.push({name: cita.nombre, start: cita.entrada, end: cita.salida, id: i} )
         } catch(e) {
           // statements
@@ -43,6 +48,29 @@
       }
     },
     methods: {
+      moveLeft(){
+
+        console.clear()
+
+
+        var view = moment(this.today).subtract(7, 'days').format("MM DD Y").split(" ")
+
+
+        console.log(view)
+
+        this.today = `${view[2]}-${view[0]}-${view[1]}`
+      },
+      moveRight() {
+        console.clear()
+
+
+        var view = moment(this.today).add(7, 'days').format("MM DD Y").split(" ")
+
+
+        console.log(view)
+
+        this.today = `${view[2]}-${view[0]}-${view[1]}`
+      },
       showEvent({ event }) {
         console.log(event);
         location.href =  `#/read/${event.id}`;
@@ -50,6 +78,7 @@
     },
     data(){
       return {
+        today: new Date(),
         dark: true,
         mode: 'week',
         events: [],
