3f2551f2f7f244a31a158e1fc5f57a413e3b1fb0
Richard <richardjimenez176@gmail.com>
2020-08-14 02:37:08 -0400

    Revert "treeshaking vuetify.js"
    This reverts commit 62be25779952ac5ef66e7b52874c2416fb1cc004.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-
 module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'www'),
@@ -11,17 +9,6 @@ module.exports = {
     module: {
         rules: [
             // ... other rules
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -34,8 +21,7 @@ module.exports = {
     },
     plugins: [
         // make sure to include the plugin!
-        new VueLoaderPlugin(),
-        new VuetifyLoaderPlugin()
+        new VueLoaderPlugin()
     ],
     entry: './src/main.js',
     output: {
